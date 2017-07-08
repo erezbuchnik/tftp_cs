9fb4771964db291edd0f3571b93328b03c9e74c2
erezbuchnik <erez@shrewdthings.com>
2017-07-08 19:17:48 +0300

    Update server_run.sh

--- server_run.sh
+++ server_run.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 set -x
 
-ethtool --offload eth0 rx off tx off
+# E.G. with csum calc: server_run.sh eth0 +c
+# E.G. without csum calc: server_run.sh eth0 -c
+
+dev=$1
+csum_offload=$2
+
+if [[ $1 =
+ethtool --offload "${dev}" rx off tx off
 cd ..
-tftp_cs/src/server +c 69 70
+tftp_cs/src/server "${csum_offload}"  69  70 &
+tftp_cs/src/server "${csum_offload}"  79  80 &
+tftp_cs/src/server "${csum_offload}"  89  90 &
+tftp_cs/src/server "${csum_offload}"  99 100 &
+tftp_cs/src/server "${csum_offload}" 109 110 &
+tftp_cs/src/server "${csum_offload}" 119 120 &
+tftp_cs/src/server "${csum_offload}" 129 130 &
 
 set +x
 
