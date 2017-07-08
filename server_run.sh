b2730bec1d5a94d9935eaf4e969706dc7c1e81eb
erezbuchnik <erez@shrewdthings.com>
2017-07-08 21:04:16 +0300

    Update server_run.sh

--- server_run.sh
+++ server_run.sh
@@ -7,6 +7,11 @@ set -x
 dev=$1
 csum_offload=$2
 
+server_name="tftp_srvr"
+cd ./src
+make
+cd ../
+
 if [ "${csum_offload}" == "+c" ]
 then
     ethtool --offload "${dev}" rx off tx off
@@ -14,14 +19,13 @@ else
     ethtool --offload "${dev}" rx on tx on
 fi
 
-cd ..
-tftp_cs/src/server "${csum_offload}"  69  70 &
-tftp_cs/src/server "${csum_offload}"  79  80 &
-tftp_cs/src/server "${csum_offload}"  89  90 &
-tftp_cs/src/server "${csum_offload}"  99 100 &
-tftp_cs/src/server "${csum_offload}" 109 110 &
-tftp_cs/src/server "${csum_offload}" 119 120 &
-tftp_cs/src/server "${csum_offload}" 129 130 &
+tftp_cs/src/${server_name} ${csum_offload}  69  70 &
+tftp_cs/src/${server_name} ${csum_offload}  79  80 &
+tftp_cs/src/${server_name} ${csum_offload}  89  90 &
+tftp_cs/src/${server_name} ${csum_offload}  99 100 &
+tftp_cs/src/${server_name} ${csum_offload} 109 110 &
+tftp_cs/src/${server_name} ${csum_offload} 119 120 &
+tftp_cs/src/${server_name} ${csum_offload} 129 130 &
 
 set +x
 
